219c6068bd254d4aa23d6173b95b0c42a12438e7
Jun Jiang <smujiang@gmail.com>
2020-09-15 21:17:55 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -6,46 +6,70 @@ import keras.backend as K
 import numpy as np
 
 from model import create_model
+from sklearn.metrics import mean_squared_error
 
 if __name__ == '__main__':
     img_rows, img_cols = 256, 256
     channel = 4
 
-    # model_weights_path = 'models/model.107-0.0197.hdf5'
-    # model = create_model()
+    model_weights_path = 'models/model.24-0.0449.hdf5'
+    model = create_model()
     #
-    # model.load_weights(model_weights_path)
-    # print(model.summary())
+    model.load_weights(model_weights_path)
+    print(model.summary())
 
-    test_path = "/infodev1/non-phi-data/junjiang/OvaryCancer/auto_enc_patches_256/OCMC-016"
-    test_images = [f for f in os.listdir(test_path) if
-                   os.path.isfile(os.path.join(test_path, f)) and f.endswith('.jpg') and "144" in f]
+    test_type_list = ["Borderline", "HighGrade"]
+    for test_type in test_type_list:
+        if test_type is "HighGrade":
+            test_path = "/infodev1/non-phi-data/junjiang/OvaryCancer/auto_enc_patches_256/OCMC-001"
+            img_fn_list = os.listdir(test_path)
+            test_images = []
+            for i_f in img_fn_list:
+                test_images.append(os.path.join(test_path, i_f))
+            testing_eval_csv = "/infodev1/non-phi-data/junjiang/Autoencoder_plus/data_log/MSE_eval_highgrade.csv"
+        elif test_type is "Borderline":
+            testing_img_fn = "/infodev1/non-phi-data/junjiang/Autoencoder_plus/data_log/test_img_fn.txt"
+            test_images = open(testing_img_fn, 'r').readlines()
 
-    samples = random.sample(test_images, 10)
+            testing_eval_csv = "/infodev1/non-phi-data/junjiang/Autoencoder_plus/data_log/MSE_eval_borderline.csv"
+        else:
+            raise Exception("undefined evaluation type")
 
-    for i in range(len(samples)):
-        image_name = samples[i]
-        filename = os.path.join(test_path, image_name)
+        samples = random.sample(test_images, 100)
+        error_list_str = "img_input,img_output,error\n"
+        for i in range(len(samples)):
+            filename = samples[i].strip()
+            # filename = os.path.join(test_path, image_name)
 
-        print('Start processing image: {}'.format(filename))
+            print('Start processing image: {}'.format(filename))
 
-        x_test = np.empty((1, img_rows, img_cols, 4), dtype=np.float32)
-        bgr_img = cv.imread(filename)
-        gray_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2GRAY)
-        rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
-        rgb_img = rgb_img / 255.
-        x_test[0, :, :, 0:3] = rgb_img
-        x_test[0, :, :, 3] = np.random.uniform(0, 1, (img_rows, img_cols))
+            x_test = np.empty((1, img_rows, img_cols, 3), dtype=np.float32)
+            bgr_img = cv.imread(filename)
+            rgb_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2RGB)
+            rgb_img = rgb_img / 255.0
+            x_test[0, :, :, :] = rgb_img
+            out = model.predict(x_test)
+            # print(out.shape)
+            out = np.squeeze(out)
+            # out = np.reshape(out, (img_rows, img_cols))
+            out = out * 255.0
 
-        out = model.predict(x_test)
-        # print(out.shape)
+            img_fn = os.path.split(filename)[1]
+            input_fn = 'images/' + img_fn.replace(".jpg", "_input.jpg")
+            output_fn = 'images/' + img_fn.replace(".jpg", "_output.jpg")
 
-        out = np.reshape(out, (img_rows, img_cols))
-        out = out * 255.0
-        out = out.astype(np.uint8)
+            mse = (np.square(rgb_img - out)).mean(axis=None)
+            error_list_str += input_fn + "," + output_fn + "," + str(mse)+"\n"
 
-        cv.imwrite('images/{}_image.png'.format(i), bgr_img)
-        cv.imwrite('images/{}_out.png'.format(i), out)
-        cv.imwrite('images/{}_gray.png'.format(i), gray_img)
+            out = out.astype(np.uint8)
+
+            bgr_out = cv.cvtColor(out, cv.COLOR_RGB2BGR)
+
+            cv.imwrite(input_fn, bgr_img)
+            cv.imwrite(output_fn, bgr_out)
+
+        fp = open(testing_eval_csv, 'w')
+        fp.write(error_list_str)
+        fp.close()
 
     K.clear_session()
